d1c8979ab015781a7863bcdb9788944da7c02440
Md. Huzaifa <saadhuzaifa2497@gmail.com>
2023-11-08 16:04:30 +0600

    firebase setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,51 @@
-
+import { Outlet, useNavigate } from 'react-router-dom'
+import { createContext, useEffect, useState } from 'react';
+import { getAuth, onAuthStateChanged, signOut, signInWithEmailAndPassword, createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import app from './firebase.init.js';
+import NavBar from './NavBar';
+import Footer from './Footer';
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+export const myContext = createContext(null)
 function App() {
-
-
+  const [user, setUser] = useState([]);
+  const navigate = useNavigate()
+  function signUpUser(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password)
+  }
+  function googlemama() {
+    return signInWithPopup(auth, provider)
+  }
+  function SignIn(email, password) {
+    return signInWithEmailAndPassword(auth, email, password)
+  }
+  function LogOut() {
+    localStorage.removeItem('cart')
+    navigate('/login')
+    return signOut(auth)
+  }
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, currentUser => {
+      setUser(currentUser);
+    });
+    return () => {
+      unSubscribe();
+    }
+  }, [])
+  const context = {
+    user,
+    signUpUser,
+    SignIn,
+    LogOut,
+    googlemama,
+  }
   return (
     <>
-      
+     <myContext.Provider value={context}>
+      <NavBar></NavBar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+      </myContext.Provider>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter,Outlet,RouterProvider,} from "react-router-dom";
+import App from './App';
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<App></App>,
+    children:[
 
+    ]
+  }
+
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+     <Outlet></Outlet>
   </React.StrictMode>,
 )

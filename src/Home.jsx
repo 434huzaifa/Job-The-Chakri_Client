82283ed17057c0bf017b5eb925ffa3554404f847
Md. Huzaifa <saadhuzaifa2497@gmail.com>
2023-11-09 08:24:35 +0600

    new job, job tap populated from database

--- src/Home.jsx
+++ src/Home.jsx
@@ -12,11 +12,21 @@ const carouselContainerStyles = {
 };
 const a = new Array(4).fill("")
 const Home = () => {
-    const [top,setTop]=useState([])
-    const caxios=useAxios()
-    useEffect(()=>{
-        caxios.get('/top').then(res=>setTop(res.data)).catch(error=>console.log(error))
-    },[])
+    const [top, setTop] = useState([])
+    const [newJob, setNewJob] = useState([])
+    const [allJob, setAllJob] = useState({})
+    const caxios = useAxios()
+    useEffect(() => {
+        caxios.get('/top').then(res => setTop(res.data)).catch(error => console.log(error))
+        caxios.get('/newjobs').then(res => {
+            // console.log(res.data)
+            setNewJob(res.data)
+        }).catch(error => console.log(error))
+        caxios.get('/jobs').then(res => {
+            // console.log(res.data)
+            setAllJob(res.data)
+        }).catch(error => console.log(error))
+    }, [])
     return (
         <div className='px-48'>
             <div className='mb-12'>
@@ -43,85 +53,105 @@ const Home = () => {
                     <TabPanel>
                         <div className=' grid grid-cols-4 gap-3 justify-items-center'>
                             {
-                                a.map(() => <JobCard flag={false}></JobCard>)
+                                allJob?.web?.map((x, index) => {
+                                    return (
+                                        <JobCard key={index} title={x.title} desc={x.desc} min={x.min} max={x.max} id={x._id} endate={x.enddate} flag={false}></JobCard>
+                                    )
+
+                                })
                             }
                         </div>
 
                     </TabPanel>
                     <TabPanel>
                         <div className=' grid grid-cols-4 gap-3 justify-items-center'>
-                            {
-                                a.map(() => <JobCard flag={false}></JobCard>)
+                        {
+                                allJob?.digital?.map((x, index) => {
+                                    return (
+                                        <JobCard key={index} title={x.title} desc={x.desc} min={x.min} max={x.max} id={x._id} endate={x.enddate} flag={false}></JobCard>
+                                    )
+
+                                })
                             }
                         </div>
                     </TabPanel>
                     <TabPanel>
                         <div className='grid grid-cols-4 gap-3 justify-items-center'>
-                            {
-                                a.map(() => <JobCard flag={false}></JobCard>)
+                        {
+                                allJob?.graphics?.map((x, index) => {
+                                    return (
+                                        <JobCard key={index} title={x.title} desc={x.desc} min={x.min} max={x.max} id={x._id} endate={x.enddate} flag={false}></JobCard>
+                                    )
+
+                                })
                             }
                         </div>
                     </TabPanel>
                 </Tabs>
             </div>
             <div className='mt-5'>
-            <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white mb-5">New Job</h5>
-            <div className='grid grid-cols-4 gap-3 justify-items-center'>
-                            {
-                                a.map(() => <JobCard></JobCard>)
+                <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white mb-5">New Job</h5>
+                <div className='grid grid-cols-4 gap-3 justify-items-center'>
+                {
+                                newJob?.map((x, index) => {
+                                    return (
+                                        <JobCard key={index} title={x.title} desc={x.desc} min={x.min} max={x.max} id={x._id} endate={x.enddate} flag={false}></JobCard>
+                                    )
+
+                                })
                             }
-                        </div>
+                </div>
             </div>
             <div className=' grid grid-cols-3 mt-5 gap-4 justify-items-center'>
                 <Card className=" w-full">
                     <div className='h-full flex justify-start flex-col'>
-                    
-                    <div className="mb-4 flex items-center justify-between">
-                        <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Latest User</h5>
-                    </div>
-                    <div className="flow-root">
-                        <ul className="divide-y divide-gray-200 dark:divide-gray-700">
-                            {
-                                top?.map((x,index)=>{
-                                    return <TopLi key={index} image={x.photo} email={x.email}  name={x.name}  ></TopLi>
-                                })
-                            }
-                        </ul>
-                    </div>
-                    </div>
-                </Card>
-                <Card className=" w-full">
-                    <div className='h-full flex justify-start flex-col'>
-                    
-                    <div className="mb-4 flex items-center justify-between">
-                        <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Top Seller</h5>
-                    </div>
-                    <div className="flow-root">
-                        <ul className="divide-y divide-gray-200 dark:divide-gray-700">
-                            {
-                                top?.map((x,index)=>{
-                                    return <TopLi key={index} image={x.photo} email={x.email}  name={x.name}  ></TopLi>
-                                })
-                            }
-                        </ul>
-                    </div>
+
+                        <div className="mb-4 flex items-center justify-between">
+                            <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Latest User</h5>
+                        </div>
+                        <div className="flow-root">
+                            <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+                                {
+                                    top?.map((x, index) => {
+                                        return <TopLi key={index} image={x.photo} email={x.email} name={x.name}  ></TopLi>
+                                    })
+                                }
+                            </ul>
+                        </div>
                     </div>
                 </Card>
                 <Card className=" w-full">
                     <div className='h-full flex justify-start flex-col'>
-                    
-                    <div className="mb-4 flex items-center justify-between">
-                        <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Top Bidder</h5>
+
+                        <div className="mb-4 flex items-center justify-between">
+                            <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Top Seller</h5>
+                        </div>
+                        <div className="flow-root">
+                            <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+                                {
+                                    top?.map((x, index) => {
+                                        return <TopLi key={index} image={x.photo} email={x.email} name={x.name}  ></TopLi>
+                                    })
+                                }
+                            </ul>
+                        </div>
                     </div>
-                    <div className="flow-root">
-                        <ul className="divide-y divide-gray-200 dark:divide-gray-700">
-                            {
-                                top?.map((x,index)=>{
-                                    return <TopLi key={index} image={x.photo} email={x.email}  name={x.name}  ></TopLi>
-                                })
-                            }
-                        </ul>
-                    </div>
+                </Card>
+                <Card className=" w-full">
+                    <div className='h-full flex justify-start flex-col'>
+
+                        <div className="mb-4 flex items-center justify-between">
+                            <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Top Bidder</h5>
+                        </div>
+                        <div className="flow-root">
+                            <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+                                {
+                                    top?.map((x, index) => {
+                                        return <TopLi key={index} image={x.photo} email={x.email} name={x.name}  ></TopLi>
+                                    })
+                                }
+                            </ul>
+                        </div>
                     </div>
                 </Card>
             </div>
--- src/JobCard.jsx
+++ src/JobCard.jsx
@@ -1,15 +1,15 @@
 import { Card } from 'flowbite-react';
 import { Button } from 'flowbite-react';
-const JobCard = ({flag}) => {
-    
+import moment from 'moment';
+const JobCard = ({flag,title,desc,min,max,endate,id}) => {
     return (
-        <Card href="#" className="max-w-sm">
+        <Card className="w-full" id={id}>
         <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Create Me a Python library
+            {title}
         </h5>
-        <p className="font-normal text-gray-700 dark:text-gray-400">Create Me a library using python that will hack nasa</p>
-        <p className="font-normal text-gray-700 dark:text-gray-400">$17-$16</p>
-        <p className="font-normal text-gray-700 dark:text-gray-400">2023-1-11</p>
+        <p className="font-normal text-gray-700 dark:text-gray-400">{String(desc).slice(0,50)}...</p>
+        <p className="font-normal text-gray-700 dark:text-gray-400">BDT {min}-{max}</p>
+        <p className="font-normal text-gray-700 dark:text-gray-400">{moment(endate,'YYYY-MM-dd').format("MMMM Do YYYY")}</p>
         {
             flag? <div className='flex gap-2'><Button color="purple">Update</Button><Button color="purple">Delete</Button></div>
             :<Button color="purple">Bid Now</Button>

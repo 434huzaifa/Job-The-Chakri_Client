ad7e4fdf54fc002b2724e2be9b98420ff861af57
huzaifa434 <saadhuzaifa2497@gmail.com>
2023-11-13 01:46:54 +0600

    refetch after update

--- src/BidRequest.jsx
+++ src/BidRequest.jsx
@@ -1,12 +1,12 @@
 import { Table, Button } from 'flowbite-react';
 import "react-step-progress-bar/styles.css";
-import { ProgressBar } from "react-step-progress-bar"
 import Slider from 'rc-slider';
 import { Spinner } from 'flowbite-react';
 import useAxios from './useAxios';
 import { useQuery } from '@tanstack/react-query';
 import { useContext } from 'react';
 import { myContext } from './App';
+import Swal from 'sweetalert2';
 const BidRequest = () => {
     const caxios = useAxios()
     const { user } = useContext(myContext)
@@ -22,6 +22,21 @@ const BidRequest = () => {
         0: 'pending',
         1: 'inprogress',
         2: 'completed'
+    }
+    function ChangeStatus(bidid, status) {
+        caxios.put(`/jobstatus/${bidid}`,{status}).then(res => {
+            if (res.data.modifiedCount==1) {
+                jobs.refetch()
+                Swal.fire("Update Successful").then(() => {
+                    // window.location.reload();
+                })
+            }else {
+                Swal.fire({
+                    title: "Update Failed",
+                    icon: "error",
+                })
+            }
+        })
     }
     return (
         <div className='mx-48'>
@@ -46,25 +61,25 @@ const BidRequest = () => {
                                 if (x.status =="progress") {
                                     s_value=1
                                 }
-                                else if (x.status=="complete") {
+                                else if (x.status=="completed") {
                                     s_value=2
                                 }
                                 return (
                                     <Table.Row key={index} className="bg-white dark:border-gray-700 dark:bg-gray-800">
                                         <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">{x.title}</Table.Cell>
-                                        <Table.Cell>{x.seller}</Table.Cell>
+                                        <Table.Cell>{x.bidder}</Table.Cell>
                                         <Table.Cell>{x.enddate}</Table.Cell>
                                         <Table.Cell>{x.status}</Table.Cell>
                                         <Table.Cell>{x.price}</Table.Cell>
                                         {
-                                            x.status == "progress" ? <Table.Cell colSpan={2} className='py-8 px-9'>
-                                                 <Slider  min={0} marks={marks} max={2} step={null} value={s_value} />
+                                            s_value != 0 ? <Table.Cell colSpan={2} className='py-8 px-9'>
+                                                 <Slider  min={0} marks={marks} dotStyle={{background:"green"}} max={2} step={null} value={s_value} />
                                             </Table.Cell> : <>
                                                 <Table.Cell>
-                                                    <Button className='mt-2' disabled={!(x.status != "rejected" && x.status == "pending")} color="purple" type="submit">Accept</Button>
+                                                    <Button onClick={()=>{ChangeStatus(x.bidid,1)}} className='mt-2' disabled={!(x.status != "rejected" && x.status == "pending")} color="purple" type="submit">Accept</Button>
                                                 </Table.Cell>
                                                 <Table.Cell>
-                                                    <Button className='mt-2' disabled={!(x.status != "rejected" && x.status == "pending")} color="purple" type="submit">Reject</Button>
+                                                    <Button onClick={()=>{ChangeStatus(x.bidid,0)}} className='mt-2' disabled={!(x.status != "rejected" && x.status == "pending")} color="purple" type="submit">Reject</Button>
                                                 </Table.Cell>
                                             </>
                                         }
